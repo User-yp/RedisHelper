a5d7504bb4b2abf5ecb5678f83d5dd227cff58db
鹏叶 <1277206411yp@gmail.com>
2024-04-23 16:51:33 +0800

    添加项目文件。

---
+++ RedisHelper.WebApi/Program.cs
@@ -0,0 +1,32 @@
+using RedisHelper;
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddRedisHelper(redisOption =>
+{
+    redisOption.ConnectionString = Environment.GetEnvironmentVariable("Redis:ConnStr");
+    redisOption.DbNumber = 0;
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ RedisHelper/RedisHelperExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RedisHelper;
+
+public static class RedisHelperExtensions
+{
+    public static IServiceCollection AddRedisHelper(this IServiceCollection services, IConfiguration configuration)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        if (configuration == null)
+            throw new ArgumentNullException(nameof(configuration));
+        services.AddOptions<RedisHelperOptions>()
+            .Configure(configuration.Bind)
+            .ValidateDataAnnotations();
+        services.AddSingleton<IRedisHelper, RedisHelper>();
+        return services;
+    }
+
+    public static IServiceCollection AddRedisHelper(this IServiceCollection services,
+        Action<RedisHelperOptions> configureOptions)
+    {
+        if (services == null)
+            throw new ArgumentNullException(nameof(services));
+
+        if (configureOptions == null)
+            throw new ArgumentNullException(nameof(configureOptions));
+
+        services.Configure(configureOptions);
+        services.AddSingleton<IRedisHelper, RedisHelper>();
+        return services;
+    }
+}
